2b3d51378d9c941bfad2f8afb4566140be02cd97
Juliano <julianodesenv@gmail.com>
2021-08-17 12:07:46 -0300

    template login and register

--- src/models/index.js
+++ src/models/index.js
@@ -2,21 +2,20 @@ const path = require('path');
 const fs = require('fs');
 const Sequelize = require('sequelize');
 const ENV = process.env.NODE_ENV || 'development';
+let sequelize = {};
 const db = {};
 
-let sequelize = {};
-
 if (ENV === 'production') {
-    sequelize = new Sequelize('mysql://root:password@127.0.0.1:3306/qa-engine', { operatorsAliases: Sequelize.Op });
+    sequelize = new Sequelize('mysql://USERNAME:PASSWORD@HOST:PORT/DB_NAME', { operatorsAliases: Sequelize.Op });
 } else {
-    sequelize = new Sequelize('mysql://root:password@127.0.0.1:3306/qa-engine', { operatorsAliases: Sequelize.Op });
+    sequelize = new Sequelize('mysql://root:password@localhost:3306/qa-engine', { operatorsAliases: Sequelize.Op });
 }
 
 fs
     .readdirSync(__dirname)
-    .filter(file => (file.indexOf('.') !== 0 && (file !== 'index.js')))
+    .filter(file => ( file.indexOf('.') !== 0 && (file !== 'index.js')))
     .forEach(file => {
-        const model = sequelize.import(path.join(__dirname, file));
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
         db[model.name] = model;
     });
 
